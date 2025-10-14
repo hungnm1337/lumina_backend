f3cf4d38bae7ee725d0de430c4dcb0a96867ddf7
Hoàng Ngọc Thuỷ <hoangthuy6326@gmail.com>
2025-10-15 00:33:02 +0700

    update speaking

--- RepositoryLayer/Exam/IExamRepository.cs
+++ RepositoryLayer/Exam/IExamRepository.cs
@@ -9,7 +9,7 @@ namespace RepositoryLayer.Exam
 {
     public interface IExamRepository
     {
-        public Task<List<ExamDTO>> GetAllExams();
+        public Task<List<ExamDTO>> GetAllExams(string? examType = null, string? partCode = null);
         public Task<ExamDTO> GetExamDetailAndExamPartByExamID(int examId);
 
         public Task<ExamPartDTO> GetExamPartDetailAndQuestionByExamPartID(int partId);
