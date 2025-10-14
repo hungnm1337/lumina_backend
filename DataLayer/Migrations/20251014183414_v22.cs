bbc69a36f23ba6e080f38eaa8ba65771d4f2d2ba
Hoàng Ngọc Thuỷ <hoangthuy6326@gmail.com>
2025-10-15 01:40:11 +0700

    fix conflicct

---
+++ DataLayer/Migrations/20251014183414_v22.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class v22 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Score",
+                table: "UserAnswers",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AnswerContent",
+                table: "UserAnswers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxQuestions",
+                table: "ExamParts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.DropColumn(
+                name: "MaxQuestions",
+                table: "ExamParts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Score",
+                table: "UserAnswers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AnswerContent",
+                table: "UserAnswers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
