66051c72ceac4b68691508787b7c064e0d8b688e
Nguyen Manh Hung <hungnm1337@gmail.com>
2025-11-26 03:00:04 +0700

    Mocktest feedback AI

---
+++ DataLayer/DTOs/MockTest/MocktestFeedbackDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DTOs.MockTest
+{
+    public class MocktestFeedbackDTO
+    {
+        public string Overview { get; set; } 
+        public int ToeicScore {get; set; }            
+        public List<string> Strengths { get; set; }    
+        public List<string> Weaknesses { get; set; }    
+        public string ActionPlan { get; set; }
+    }
+}
